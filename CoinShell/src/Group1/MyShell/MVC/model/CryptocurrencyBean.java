7f982e2e4f677bbe1eec07081284906238eb043d
Hoxton <za546321@yahoo.com.tw>
2022-04-29 12:21:07 +0800

    資料庫抓取更新

--- CoinShell/src/Group1/MyShell/MVC/model/CryptocurrencyBean.java
+++ CoinShell/src/Group1/MyShell/MVC/model/CryptocurrencyBean.java
@@ -2,6 +2,8 @@ package Group1.MyShell.MVC.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,10 @@ import javax.persistence.Table;
 @Table(name = "Cryptocurrency")
 public class CryptocurrencyBean {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+
 	@Column(name = "Date")
 	private String date;
 
