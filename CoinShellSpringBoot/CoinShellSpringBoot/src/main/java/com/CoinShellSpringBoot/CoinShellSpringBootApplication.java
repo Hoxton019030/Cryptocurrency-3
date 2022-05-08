40e1aef9e43d26e5d009dcf59764e3881b1470c7
Hoxton <za546321@yahoo.com.tw>
2022-05-08 14:21:22 +0800

    加入SpringBoot專案

---
+++ CoinShellSpringBoot/CoinShellSpringBoot/src/main/java/com/CoinShellSpringBoot/CoinShellSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.CoinShellSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinShellSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinShellSpringBootApplication.class, args);
+	}
+
+}
---
+++ CoinShellSpringBoot/CoinShellSpringBoot/src/test/java/com/CoinShellSpringBoot/CoinShellSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoinShellSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinShellSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
